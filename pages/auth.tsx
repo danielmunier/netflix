34ab9ab1e4df387c11e090bb3d994d8502054b89
Daniel <dmunier27@gmail.com>
2024-01-11 01:00:34 -0300

    new: recruiter button

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -53,6 +53,21 @@ const login = useCallback(async()=> {
 }, [email, password])
 
 
+const recruiterLogin = useCallback(async () => {
+  try {
+    await signIn("credentials", {
+      email: "recruiter@gmail.com",
+      password: "123",
+      redirect: true,
+      callbackUrl: "/profiles",
+    });
+    console.log("Successful recruiter login");
+  } catch (e) {
+    console.log("Error trying to login as recruiter");
+    console.log(e);
+  }
+}, []);
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -94,15 +109,23 @@ const login = useCallback(async()=> {
             <button onClick={variant === "login" ? login:register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover: bg-red-700 transition">
               {variant === "login" ? "Login" : "Register"}
             </button>
+        
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div onClick={() => signIn('google', {callbackUrl: "/profiles"})} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer h over:opacity-80 transition"> <FcGoogle size={30} /></div>
               <div onClick={() => signIn('github', {callbackUrl: "/profiles"})} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer h over:opacity-80 transition"> <FaGithub size={30} /></div>
             </div>
-            <p className="text-neutral-500 mt-12">
+          
+            <p className="text-neutral-500 mt-5">
               {variant === 'login' ? "Don't have an account? " : "Already have an account?"}
               <span onClick={toggleVariant} className="text-white cursor-pointer">
                 {variant === 'login' ? 'Sign up' : 'Login'}
               </span>
+            </p>
+              
+            <p className="text-neutral-500 mt-5">
+              Are you a recruiter?
+            <span onClick={recruiterLogin} className="text-white cursor-pointer"> Sign in instantly</span>
+
             </p>
           </div>
         </div>
